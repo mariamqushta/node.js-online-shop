ab45eb780b70eae057b89c81eaa53dca74832de4
mariamqushta <154238625+mariamqushta@users.noreply.github.com>
2025-12-23 16:34:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import serverless from "serverless-http";
+
 import { ConnectToDB } from "./DB/mongoose.js";
-import authRouter from "./routes/auth-routes.js"
+import authRouter from "./routes/auth-routes.js";
 import productRouter from "./routes/product-routes.js";
 
 dotenv.config();
 
 const app = express();
 
-// Allow your frontend origin + localhost for dev
 app.use(cors({
-  origin: ["http://localhost:3000", "https://your-frontend.vercel.app"],
-  credentials: true
+  origin: ["http://localhost:3000", "https://react-online-shop-xzgh.vercel.app/register"],
+  credentials: true,
 }));
 
 app.use(express.json());
@@ -29,7 +28,13 @@ app.get("/", (req, res) => {
   res.json({ message: "API is running 🚀" });
 });
 
-// Connect once to DB
-ConnectToDB();
+let isConnected = false;
 
-export default serverless(app);
+export default async function handler(req, res) {
+  if (!isConnected) {
+    await ConnectToDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
+
